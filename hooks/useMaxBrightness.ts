2a63a67a7d58e1c74d6543a462d97b469c2655a5
THE-lulu57 <197119141+THE-lulu57@users.noreply.github.com>
2026-01-31 19:53:47 +0100

    Refactor useMaxBrightness for clarity and efficiency
    Removed unused timeout reference and isMounted flag. Simplified brightness management logic.

--- hooks/useMaxBrightness.ts
+++ hooks/useMaxBrightness.ts
@@ -6,58 +6,40 @@ import { warn } from "@/utils/logger/logger";
 export function useMaxBrightness() {
 
   const previousBrightness = useRef<number | null>(null);
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    let isMounted = true;
-
     const enableBrightness = async () => {
       try {
-        const current = await getBrightnessAsync();
-
-        if (!isMounted) return;
-
         if (previousBrightness.current === null) {
-          previousBrightness.current = current
+          previousBrightness.current = await getBrightnessAsync();
         }
         await setBrightnessAsync(1);
       } catch (error) {
-        warn("Failed to set brightness");
+        warn("Failed to set brightness:");
       }
     };
 
     const restoreBrightness = async () => {
       try {
-        if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-        timeoutRef.current = null;
-      }
         if (previousBrightness.current !== null) {
           await setBrightnessAsync(previousBrightness.current);
         }
       } catch (error) {
-        warn("Failed to restore brightness");
+        warn("Failed to restore brightness:");
       }
     };
 
-    timeoutRef.current = setTimeout(() => {
-      if (isMounted) {
-        enableBrightness();
-      }
-    }, 100);
+    enableBrightness();
 
     const subscription = AppState.addEventListener("change", (nextAppState) => {
       if (nextAppState === "inactive" || nextAppState === "background" ) {
         restoreBrightness();
       } else if (nextAppState === "active") {
-        timeoutRef.current = setTimeout(() => {
-          if (isMounted) enableBrightness();
-        }, 100);
+        enableBrightness();
       }
     });
 
     return () => {
-      isMounted = false;
       restoreBrightness();
       subscription.remove();
     };
