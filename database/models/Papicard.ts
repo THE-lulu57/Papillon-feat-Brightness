1a011122d53b697b6e1d13beee3ef75089db01ae
THE-lulu57 <197119141+THE-lulu57@users.noreply.github.com>
2026-02-21 20:56:38 +0100

    feat(papicard): add a model for papicard

---
+++ database/models/Papicard.ts
@@ -0,0 +1,12 @@
+import { Model } from "@nozbe/watermelondb";
+import { field } from "@nozbe/watermelondb/decorators";
+
+export default class Papicard extends Model {
+  static table = 'papicard';
+
+  @field('label') label!: string;
+  @field('type') type!: string;
+  @field('data') data!: string;
+  @field('color') color!: string;
+}
+
