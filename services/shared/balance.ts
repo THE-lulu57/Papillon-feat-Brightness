a46145ca7e8352654a672920d29e42776f2188c2
THE-lulu57 <197119141+THE-lulu57@users.noreply.github.com>
2026-04-26 16:59:22 +0200

    feat(papicard): Add optional serviceId property to Balance interface

--- services/shared/balance.ts
+++ services/shared/balance.ts
@@ -1,6 +1,8 @@
 import { GenericInterface } from "./types";
+import { Services } from "@/stores/account/types";
 
 export interface Balance extends GenericInterface {
+	serviceId?: Services;
 	amount: number;
 	currency: string;
 	lunchRemaining: number;
